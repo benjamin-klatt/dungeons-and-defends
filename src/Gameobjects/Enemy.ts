d76444ed17d8d0f8fec1af015d53ba925a8e282e
Faustus <niels.f.guettler@gmail.com>
2023-02-07 18:54:03 +0000

    Update Enemy.ts and Map.ts

--- src/Gameobjects/Enemy.ts
+++ src/Gameobjects/Enemy.ts
@@ -5,12 +5,13 @@ export class Enemy extends Gameobject {
     //Eigenschaften
     life: number = 0;
     speed: number = 0;
-    xPos: number = 0;
-    yPos: number = 0;
+    xPos: number = 20;
+    yPos: number = 20;
     xPosGoal: number = 0;
     yPosGoal: number = 0;
-    bounty:number = 0;
-     map:Map;
+    bounty: number = 0;
+    map:Map;
+    cpNumber: number = 0;
     
     constructor(map:Map){
         super()
@@ -19,7 +20,7 @@ export class Enemy extends Gameobject {
     
     render(time:number, ctx: CanvasRenderingContext2D) {
         ctx.fillStyle = "lime";
-        ctx.fillRect(20, 20, 10, 10);
+        ctx.fillRect(this.xPos, this.yPos, 10, 10);
     }
     
 }
