70d955e5c0d573b7189a0fa376922eb7c41830c9
Viggo Kuerten <viggo.kuerten@gmail.com>
2023-02-07 18:23:16 +0000

    kleinigkeiten

--- src/Gameobjects/Map.ts
+++ src/Gameobjects/Map.ts
@@ -3,9 +3,12 @@ import { Gameobject } from "../Gameobject";
 let mapImage = document.getElementById("map") as HTMLImageElement;
 
 export class Map extends Gameobject {
-  render(time: number, ctx: CanvasRenderingContext2D) {
-    ctx.drawImage(mapImage, 0, 0);
-    console.log(time)
+  public field: Array<Array<Field>> = []
+  constructor(){
     
   }
+  render(time: number, ctx: CanvasRenderingContext2D) {
+    ctx.drawImage(mapImage, 0, 0);
+  }
+  
 }
